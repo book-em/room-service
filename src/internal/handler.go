a445237274463a7b9a7a1bb50b95234d36f2d9a2
Vasilije <vasilijezzzz@gmail.com>
2025-08-11 17:44:42 +0200

    fix: Merge `route` and `handler` into `handler.go`

--- src/internal/handler.go
+++ src/internal/handler.go
@@ -1,9 +1,25 @@
 package internal
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
 type Handler struct {
 	service Service
 }
 
 func NewHandler(s Service) Handler {
 	return Handler{s}
+}
+
+type Route struct {
+	handler Handler
+}
+
+func NewRoute(handler Handler) *Route {
+	return &Route{handler}
+}
+
+func (r *Route) Route(rg *gin.RouterGroup) {
+
 }
