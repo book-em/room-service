cded9447b8f4f792d47ae0c5f13f01422e098ee5
magley <60552482+magley@users.noreply.github.com>
2025-08-12 16:51:59 +0200

    test: Add utility functions for integration tests

--- src/client/userclient/model.go
+++ src/client/userclient/model.go
@@ -7,3 +7,22 @@ const (
 	Host  UserRole = "host"
 	Admin UserRole = "admin"
 )
+
+type UserCreateDTO struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"   `
+	Name     string `json:"name"    `
+	Surname  string `json:"surname" `
+	Address  string `json:"address" `
+	Role     string `json:"role"    `
+}
+
+type LoginDTO struct {
+	UsernameOrEmail string `json:"usernameOrEmail"`
+	Password        string `json:"password"`
+}
+
+type JWTDTO struct {
+	Jwt string `json:"jwt"`
+}
