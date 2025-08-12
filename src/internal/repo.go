42895a45c5ade904be08af664daf05811f29d029
magley <60552482+magley@users.noreply.github.com>
2025-08-12 11:17:28 +0200

    feat: Add service and repo methods for `find by id` and `create`

--- src/internal/repo.go
+++ src/internal/repo.go
@@ -5,6 +5,8 @@ import (
 )
 
 type Repository interface {
+	Create(room *Room) error
+	FindById(id uint) (*Room, error)
 }
 
 type repository struct {
@@ -13,4 +15,17 @@ type repository struct {
 
 func NewRepository(db *gorm.DB) Repository {
 	return &repository{db}
+}
+
+func (r *repository) Create(room *Room) error {
+	return r.db.Create(room).Error
+}
+
+func (r *repository) FindById(id uint) (*Room, error) {
+	var room Room
+	err := r.db.Where("id = ?", id).First(&room).Error
+	if err != nil {
+		return nil, err
+	}
+	return &room, nil
 }
