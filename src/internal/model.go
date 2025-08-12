10389454e767356d795f6830bee57e8d2426d7fa
magley <60552482+magley@users.noreply.github.com>
2025-08-12 11:14:05 +0200

    feat: Create room model

--- src/internal/model.go
+++ src/internal/model.go
@@ -1,1 +1,13 @@
 package internal
+
+type Room struct {
+	ID          uint     `json:"id"       gorm:"primaryKey"`
+	HostID      uint     `json:"hostID"`
+	Name        string   `json:"name"     gorm:"type:varchar(50);not null"`
+	Description string   `json:"description"`
+	Address     string   `json:"address"  gorm:"type:varchar(150);not null"`
+	MinGuests   uint     `json:"minGuests"`
+	MaxGuests   uint     `json:"maxGuests"`
+	Photos      []string `json:"photos"   gorm:"type:text;serializer:json"`
+	Commodities []string `json:"commodities" gorm:"type:text;serializer:json"`
+}
