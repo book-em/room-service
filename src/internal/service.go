16de3be1b11d6b9f601676c63b7da1a046c0d83c
magley <60552482+magley@users.noreply.github.com>
2025-08-12 11:17:28 +0200

    feat: Add service and repo methods for `find by id` and `create`

--- src/internal/service.go
+++ src/internal/service.go
@@ -1,6 +1,10 @@
 package internal
 
+import "fmt"
+
 type Service interface {
+	Create(dto CreateRoomDTO) (*Room, error)
+	FindById(id uint) (*Room, error)
 }
 
 type service struct {
@@ -9,4 +13,34 @@ type service struct {
 
 func NewService(r Repository) Service {
 	return &service{r}
+}
+
+func (s *service) Create(dto CreateRoomDTO) (*Room, error) {
+	var photos []string // TODO: Actually save photos and fetch the filenames
+
+	room := &Room{
+		HostID:      dto.HostID,
+		Name:        dto.Name,
+		Description: dto.Description,
+		Address:     dto.Address,
+		MinGuests:   dto.MinGuests,
+		MaxGuests:   dto.MaxGuests,
+		Photos:      photos,
+		Commodities: dto.Commodities,
+	}
+
+	err := s.repo.Create(room)
+	if err != nil {
+		return nil, err
+	}
+
+	return room, nil
+}
+
+func (s *service) FindById(id uint) (*Room, error) {
+	room, err := s.repo.FindById(id)
+	if err != nil {
+		return nil, fmt.Errorf("Not found")
+	}
+	return room, nil
 }
