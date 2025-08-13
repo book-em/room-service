92ace78e6c5be2d67901fcd474d7b888ae7466cd
magley <60552482+magley@users.noreply.github.com>
2025-08-13 12:23:48 +0200

    test: Add more integration tests

--- src/client/userclient/client.go
+++ src/client/userclient/client.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
@@ -22,18 +23,29 @@ func NewUserClient() UserClient {
 }
 
 func (c *userClient) FindById(id uint) (*UserDTO, error) {
+	log.Printf("Find user %d", id)
+
 	resp, err := http.Get(fmt.Sprintf("%s/%d", c.baseURL, id))
+
 	if err != nil {
+		log.Printf("Error %v", err)
 		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("User %d not found: http %d", id, resp.StatusCode)
+		return nil, fmt.Errorf("user %d not found", id)
 	}
 
 	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
+		log.Printf("Parsing response error: %v", id, err)
 		return nil, err
 	}
 
 	var obj UserDTO
 	if err := json.Unmarshal(bodyBytes, &obj); err != nil {
+		log.Printf("JSON Unmarshall error: %v", id, err)
 		return nil, err
 	}
 
--- src/internal/middleware.go
+++ src/internal/middleware.go
@@ -1,1 +1,47 @@
 package internal
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type APIError struct {
+	Code    int
+	Message string
+}
+
+func (e *APIError) Error() string {
+	return e.Message
+}
+
+func ErrNotFound(resourceName string, id uint) *APIError {
+	return &APIError{
+		Code:    http.StatusNotFound,
+		Message: fmt.Sprintf("Resource %s with id %d not found", resourceName, id),
+	}
+}
+
+var (
+	ErrUnauthorized = &APIError{Code: http.StatusUnauthorized, Message: "Unauthorized"}
+	ErrBadRequest   = &APIError{Code: http.StatusBadRequest, Message: "Bad request"}
+)
+
+func MapErrorToHTTP(err error) (int, string) {
+	switch e := err.(type) {
+	case *APIError:
+		return e.Code, e.Message
+	default:
+		return http.StatusInternalServerError, fmt.Sprintf("Internal server error: %v", err)
+	}
+}
+
+func AbortError(c *gin.Context, err error) {
+	status, message := MapErrorToHTTP(err)
+	c.AbortWithStatusJSON(status, gin.H{
+		"error": message,
+	})
+	log.Printf("[ERROR] %s, returning HTTP %d, %v", message, status, err)
+}
