da6b873677a5efd2b5102f562778ce176aa6e83d
magley <60552482+magley@users.noreply.github.com>
2025-08-13 10:24:45 +0200

    test: Write unit tests for `Find*` methods

---
+++ src/test/unit/find_by_host_test.go
@@ -0,0 +1,82 @@
+package test
+
+import (
+	"bookem-room-service/internal"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_FindByHost_Success(t *testing.T) {
+	svc, mockRepo, mockUserClient := createTestRoomService()
+
+	host := DefaultUser_Host
+
+	room1 := DefaultRoom
+	room2 := DefaultRoom
+	room3 := DefaultRoom
+	room1.HostID = host.Id
+	room2.HostID = host.Id
+	room3.HostID = host.Id
+
+	rooms := []internal.Room{*room1, *room2, *room3}
+
+	mockUserClient.On("FindById", host.Id).Return(host, nil)
+	mockRepo.On("FindByHost", host.Id).Return(rooms, nil)
+
+	roomsGot, err := svc.FindByHost(host.Id)
+
+	assert.NoError(t, err)
+	assert.Equal(t, rooms, roomsGot)
+	mockUserClient.AssertNumberOfCalls(t, "FindById", 1)
+	mockUserClient.AssertExpectations(t)
+	mockRepo.AssertNumberOfCalls(t, "FindByHost", 1)
+	mockRepo.AssertExpectations(t)
+}
+
+func Test_FindByHost_UserNotFound(t *testing.T) {
+	svc, _, mockUserClient := createTestRoomService()
+
+	hostId := uint(123)
+	mockUserClient.On("FindById", hostId).Return(nil, fmt.Errorf("user not found"))
+
+	roomsGot, err := svc.FindByHost(hostId)
+
+	assert.Error(t, err)
+	assert.Nil(t, roomsGot)
+	mockUserClient.AssertNumberOfCalls(t, "FindById", 1)
+	mockUserClient.AssertExpectations(t)
+}
+
+func Test_FindByHost_UserNotHost(t *testing.T) {
+	svc, _, mockUserClient := createTestRoomService()
+
+	notHost := DefaultUser_Guest
+	mockUserClient.On("FindById", notHost.Id).Return(notHost, nil)
+
+	roomsGot, err := svc.FindByHost(notHost.Id)
+
+	assert.Error(t, err)
+	assert.Nil(t, roomsGot)
+	mockUserClient.AssertNumberOfCalls(t, "FindById", 1)
+	mockUserClient.AssertExpectations(t)
+}
+
+func Test_FindByHost_DbError(t *testing.T) {
+	svc, mockRepo, mockUserClient := createTestRoomService()
+
+	host := DefaultUser_Host
+
+	mockUserClient.On("FindById", host.Id).Return(host, nil)
+	mockRepo.On("FindByHost", host.Id).Return(nil, fmt.Errorf("db error"))
+
+	roomsGot, err := svc.FindByHost(host.Id)
+
+	assert.Error(t, err)
+	assert.Nil(t, roomsGot)
+	mockUserClient.AssertNumberOfCalls(t, "FindById", 1)
+	mockUserClient.AssertExpectations(t)
+	mockRepo.AssertNumberOfCalls(t, "FindByHost", 1)
+	mockRepo.AssertExpectations(t)
+}
