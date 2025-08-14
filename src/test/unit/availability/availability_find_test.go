56062bf675096367235bd36066465a5fd2910ff3
magley <60552482+magley@users.noreply.github.com>
2025-08-14 16:42:27 +0200

    test: Write unit tests for room availability

---
+++ src/test/unit/availability/availability_find_test.go
@@ -0,0 +1,122 @@
+package test
+
+import (
+	"bookem-room-service/internal"
+	. "bookem-room-service/test/unit"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_FindAvailabilityListById_Success(t *testing.T) {
+	svc, _, mockAvailRepo, _ := CreateTestRoomService()
+
+	li := DefaultAvailabilityList
+
+	mockAvailRepo.On("FindListById", li.ID).Return(li, nil)
+
+	liGot, err := svc.FindAvailabilityListById(li.ID)
+
+	assert.NoError(t, err)
+	assert.Equal(t, li, liGot)
+	mockAvailRepo.AssertNumberOfCalls(t, "FindListById", 1)
+	mockAvailRepo.AssertExpectations(t)
+}
+
+func Test_FindAvailabilityListById_NotFound(t *testing.T) {
+	svc, _, mockAvailRepo, _ := CreateTestRoomService()
+
+	li := DefaultAvailabilityList
+
+	mockAvailRepo.On("FindListById", li.ID).Return(nil, fmt.Errorf("not found"))
+
+	liGot, err := svc.FindAvailabilityListById(li.ID)
+
+	assert.Error(t, err)
+	assert.Nil(t, liGot)
+	mockAvailRepo.AssertNumberOfCalls(t, "FindListById", 1)
+	mockAvailRepo.AssertExpectations(t)
+}
+
+func Test_FindAvailabilityListsByRoomId_Success(t *testing.T) {
+	svc, _, mockAvailRepo, _ := CreateTestRoomService()
+
+	li1 := *DefaultAvailabilityList
+	li2 := *DefaultAvailabilityList
+	li1.ID = 1
+	li2.ID = 2
+	li1.RoomID = 1
+	li2.RoomID = 2
+
+	lists := []internal.RoomAvailabilityList{li1, li2}
+
+	mockAvailRepo.On("FindListsByRoomId", li1.RoomID).Return(lists, nil)
+
+	listsGot, err := svc.FindAvailabilityListsByRoomId(li1.RoomID)
+
+	assert.NoError(t, err)
+	assert.Equal(t, lists, listsGot)
+	mockAvailRepo.AssertNumberOfCalls(t, "FindListsByRoomId", 1)
+	mockAvailRepo.AssertExpectations(t)
+}
+
+func Test_FindAvailabilityListsByRoomId_Success_NoLists(t *testing.T) {
+	svc, _, mockAvailRepo, _ := CreateTestRoomService()
+
+	lists := []internal.RoomAvailabilityList{}
+
+	mockAvailRepo.On("FindListsByRoomId", uint(999)).Return(lists, nil)
+
+	listsGot, err := svc.FindAvailabilityListsByRoomId(uint(999))
+
+	assert.NoError(t, err)
+	assert.Equal(t, lists, listsGot)
+	mockAvailRepo.AssertNumberOfCalls(t, "FindListsByRoomId", 1)
+	mockAvailRepo.AssertExpectations(t)
+}
+
+func Test_FindAvailabilityListsByRoomId_NotFound(t *testing.T) {
+	svc, _, mockAvailRepo, _ := CreateTestRoomService()
+
+	li := DefaultAvailabilityList
+
+	mockAvailRepo.On("FindListsByRoomId", li.RoomID).Return(nil, fmt.Errorf("not found"))
+
+	liGot, err := svc.FindAvailabilityListsByRoomId(li.RoomID)
+
+	assert.Error(t, err)
+	assert.Nil(t, liGot)
+	mockAvailRepo.AssertNumberOfCalls(t, "FindListsByRoomId", 1)
+	mockAvailRepo.AssertExpectations(t)
+}
+
+func Test_FindCurrentAvailabilityListOfRoom_Success(t *testing.T) {
+	svc, _, mockAvailRepo, _ := CreateTestRoomService()
+
+	li := DefaultAvailabilityList
+
+	mockAvailRepo.On("FindCurrentListOfRoom", li.RoomID).Return(li, nil)
+
+	liGot, err := svc.FindCurrentAvailabilityListOfRoom(li.RoomID)
+
+	assert.NoError(t, err)
+	assert.Equal(t, li, liGot)
+	mockAvailRepo.AssertNumberOfCalls(t, "FindCurrentListOfRoom", 1)
+	mockAvailRepo.AssertExpectations(t)
+}
+
+func Test_FindCurrentAvailabilityListOfRoom_NotFound(t *testing.T) {
+	svc, _, mockAvailRepo, _ := CreateTestRoomService()
+
+	li := DefaultAvailabilityList
+
+	mockAvailRepo.On("FindCurrentListOfRoom", li.RoomID).Return(nil, fmt.Errorf("not found"))
+
+	liGot, err := svc.FindCurrentAvailabilityListOfRoom(li.RoomID)
+
+	assert.Error(t, err)
+	assert.Nil(t, liGot)
+	mockAvailRepo.AssertNumberOfCalls(t, "FindCurrentListOfRoom", 1)
+	mockAvailRepo.AssertExpectations(t)
+}
