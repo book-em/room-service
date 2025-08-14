022e8ab05c9761e13779409b96f53d431cd1042a
magley <60552482+magley@users.noreply.github.com>
2025-08-14 16:41:21 +0200

    feat: Add DTO structs for room availability

--- src/internal/dto.go
+++ src/internal/dto.go
@@ -1,5 +1,7 @@
 package internal
 
+import "time"
+
 type RoomDTO struct {
 	ID          uint     `json:"id"`
 	HostID      uint     `json:"hostID"`
@@ -10,6 +12,17 @@ type RoomDTO struct {
 	MaxGuests   uint     `json:"maxGuests"`
 	Photos      []string `json:"photos"`
 	Commodities []string `json:"commodities"`
+}
+
+type CreateRoomDTO struct {
+	HostID        uint     `json:"hostID"`
+	Name          string   `json:"name"`
+	Description   string   `json:"description"`
+	Address       string   `json:"address"`
+	MinGuests     uint     `json:"minGuests"`
+	MaxGuests     uint     `json:"maxGuests"`
+	PhotosPayload []string `json:"photosPayload"`
+	Commodities   []string `json:"commodities"`
 }
 
 func NewRoomDTO(r *Room) RoomDTO {
@@ -26,13 +39,50 @@ func NewRoomDTO(r *Room) RoomDTO {
 	}
 }
 
-type CreateRoomDTO struct {
-	HostID        uint     `json:"hostID"`
-	Name          string   `json:"name"`
-	Description   string   `json:"description"`
-	Address       string   `json:"address"`
-	MinGuests     uint     `json:"minGuests"`
-	MaxGuests     uint     `json:"maxGuests"`
-	PhotosPayload []string `json:"photosPayload"`
-	Commodities   []string `json:"commodities"`
+type CreateRoomAvailabilityListDTO struct {
+	RoomID uint                            `json:"roomId"`
+	Items  []CreateRoomAvailabilityItemDTO `json:"items"`
+}
+
+type RoomAvailabilityListDTO struct {
+	ID            uint                      `json:"id"`
+	RoomID        uint                      `json:"roomId"`
+	EffectiveFrom time.Time                 `json:"effectiveFrom"`
+	Items         []RoomAvailabilityItemDTO `json:"items"`
+}
+
+func NewRoomAvailabilityListDTO(list *RoomAvailabilityList) RoomAvailabilityListDTO {
+	items := make([]RoomAvailabilityItemDTO, 0, len(list.Items))
+	for _, item := range list.Items {
+		items = append(items, NewRoomAvailabilityItemDTO(item))
+	}
+
+	return RoomAvailabilityListDTO{
+		ID:            list.ID,
+		RoomID:        list.RoomID,
+		EffectiveFrom: list.EffectiveFrom,
+		Items:         items,
+	}
+}
+
+type RoomAvailabilityItemDTO struct {
+	ID        uint      `json:"id"`
+	DateFrom  time.Time `json:"dateFrom"`
+	DateTo    time.Time `json:"dateTo"`
+	Available bool      `json:"available"`
+}
+
+type CreateRoomAvailabilityItemDTO struct {
+	DateFrom  time.Time `json:"dateFrom"`
+	DateTo    time.Time `json:"dateTo"`
+	Available bool      `json:"available"`
+}
+
+func NewRoomAvailabilityItemDTO(item RoomAvailabilityItem) RoomAvailabilityItemDTO {
+	return RoomAvailabilityItemDTO{
+		ID:        item.ID,
+		DateFrom:  item.DateFrom,
+		DateTo:    item.DateTo,
+		Available: item.Available,
+	}
 }
