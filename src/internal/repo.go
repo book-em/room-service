134ea8dc3875a1d9cac658cd78716f427cb5182a
Vasilije <vasilijezzzz@gmail.com>
2025-10-28 08:01:52 +0100

    feat: Add `DeleteRoomsByHostId` to support deletion via user service

--- src/internal/repo.go
+++ src/internal/repo.go
@@ -11,6 +11,7 @@ type Repository interface {
 	FindById(id uint) (*Room, error)
 	FindByHost(hostId uint) ([]Room, error)
 	FindByFilters(guestsNumber uint, address string) ([]Room, error)
+	DeleteRoomsByHostId(hostId uint) error
 }
 
 type repository struct {
@@ -31,6 +32,14 @@ func (r *repository) Update(room *Room) error {
 
 func (r *repository) Delete(room *Room) error {
 	return r.db.Delete(&Room{}, room.ID).Error
+}
+
+func (r *repository) DeleteRoomsByHostId(hostId uint) error {
+	err := r.db.Model(&Room{}).Where("host_id = ?", hostId).Update("deleted", true).Error
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *repository) FindById(id uint) (*Room, error) {
